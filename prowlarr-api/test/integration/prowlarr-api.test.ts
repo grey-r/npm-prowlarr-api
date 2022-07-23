4aeebe3fd8d59859ff09be47a950fd848b8268ef
Grey Ruessler <greyruessler@gmail.com>
2022-07-22 20:39:23 -0500

    repair test

--- prowlarr-api/test/integration/prowlarr-api.test.ts
+++ prowlarr-api/test/integration/prowlarr-api.test.ts
@@ -6,5 +6,5 @@ test('Health GET test', async () => {
     process.env.BASE_URL ?? '',
     process.env.API_KEY ?? ''
   );
-  expect(await HealthService.getHealth()).toBeFalsy();
+  expect(await HealthService.getHealth()).toBeTruthy();
 });
