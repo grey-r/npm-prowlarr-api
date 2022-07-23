75595dd4418aed56d9232c769b0f60a3745afcac
Grey Ruessler <greyruessler@gmail.com>
2022-07-23 11:58:00 -0500

    fix docker-compose baseurl

--- prowlarr-api/test/integration/prowlarr-api.test.ts
+++ prowlarr-api/test/integration/prowlarr-api.test.ts
@@ -1,10 +1,9 @@
 import 'dotenv/config';
 import {HealthService} from '../../src/prowlarr-api';
 
+HealthService.configure(process.env.BASE_URL ?? '', process.env.API_KEY ?? '');
+
 test('Health GET test', async () => {
-  HealthService.configure(
-    process.env.BASE_URL ?? '',
-    process.env.API_KEY ?? ''
-  );
-  expect(await HealthService.getHealth()).toBeTruthy();
+  const res = await HealthService.getHealth();
+  expect(res).toBeTruthy();
 });
